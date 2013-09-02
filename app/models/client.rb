23c1219751ea7f9f4306ced9fce6075327ce5602
Amanda <amanda.n.krauss@gmail.com>
2013-09-02 16:11:09 -0500

    field test

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,4 @@
 class Client < ActiveRecord::Base
   attr_accessible :email, :first_name, :formula, :last_name, :phone, :user_id
+  validates_presence_of :email, :first_name, :formula, :last_name, :phone, :user_id
 end
--- test/unit/client_test.rb
+++ test/unit/client_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class ClientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid with all attributes" do
+    c = clients(:one)
+    assert c.valid?, "field is missing"
+  end
 end
