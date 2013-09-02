d60a14e40413e6c926c26cefd6882275d2a75fa3
Amanda <amanda.n.krauss@gmail.com>
2013-09-02 09:02:15 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BettyApp::Application.config.secret_token = 'f62ed6b71516373316ea5b4cc3a877a89c3f77f845c5f63dc7453b0e00789fbb7b0a220fec047ef7fd945a09977ef3756b15a03f97322c2a109e6c28f8b667da'
