8e2b133cb6f5ab0a8077e9ecb469d7df704c01d3
Amanda <amanda.n.krauss@gmail.com>
2013-09-11 08:39:47 -0500

    update client form views for nested resources

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -19,7 +19,7 @@ class ClientsController < ApplicationController
   # GET /clients/1
   # GET /clients/1.json
   def show
-    @client = @user.Client.find(params[:id])
+    @client = @user.clients.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
@@ -30,7 +30,7 @@ class ClientsController < ApplicationController
   # GET /clients/new
   # GET /clients/new.json
   def new
-    @client = @user.Client.new
+    @client = @user.clients.new
 
     respond_to do |format|
       format.html # new.html.erb
@@ -40,13 +40,13 @@ class ClientsController < ApplicationController
 
   # GET /clients/1/edit
   def edit
-    @client = @user.Client.find(params[:id])
+    @client = @user.clients.find(params[:id])
   end
 
   # POST /clients
   # POST /clients.json
   def create
-    @client = @uesr.Client.new(params[:client])
+    @client = @uesr.clients.new(params[:client])
 
     respond_to do |format|
       if @client.save
@@ -62,11 +62,11 @@ class ClientsController < ApplicationController
   # PUT /clients/1
   # PUT /clients/1.json
   def update
-    @client = @user.Client.find(params[:id])
+    @client = @user.clients.find(params[:id])
 
     respond_to do |format|
       if @client.update_attributes(params[:client])
-        format.html { redirect_to @client, notice: 'Client was successfully updated.' }
+        format.html { redirect_to user_clients_url, notice: 'Client was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
@@ -78,11 +78,11 @@ class ClientsController < ApplicationController
   # DELETE /clients/1
   # DELETE /clients/1.json
   def destroy
-    @client = Client.find(params[:id])
+    @client = @user.client.find(params[:id])
     @client.destroy
 
     respond_to do |format|
-      format.html { redirect_to clients_url }
+      format.html { redirect_to user_clients_url }
       format.json { head :no_content }
     end
   end
