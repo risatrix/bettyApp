a27117fc4b2d2147c521f398105a0a50295d36fd
Amanda <amanda.n.krauss@gmail.com>
2013-09-11 08:48:22 -0500

    add message after destroy command

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -78,11 +78,11 @@ class ClientsController < ApplicationController
   # DELETE /clients/1
   # DELETE /clients/1.json
   def destroy
-    @client = @user.client.find(params[:id])
+    @client = @user.clients.find(params[:id])
     @client.destroy
 
     respond_to do |format|
-      format.html { redirect_to user_clients_url }
+      format.html { redirect_to user_clients_url, notice: 'Client removed from list.' }
       format.json { head :no_content }
     end
   end
